bae6c5140083cd030128bd6ae5d1853d383f6ce0
LikeRiver <137577102@qq.com>
2013-09-07 19:24:53 +0800

    file text
    there are some questions
    remember email teacher~

---
+++ file/helloworld/helloworld/helloworld.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<fstream>
+
+using namespace std;
+
+int main (){
+	char line[180];
+	fstream myfile_in;
+	ifstream myfile_out;
+	myfile_in.open("H:\\work\\compiler\\VS2008\\mycode\\helloworld\\helloworld\\in.txt",ios::out|ios::trunc);
+	myfile_out.open("H:\\work\\compiler\\VS2008\\mycode\\helloworld\\helloworld\\out.txt");
+	if((!myfile_in)||(!myfile_out)){
+		cerr<<"Error!!!"<<endl;
+		exit(1);
+	}
+	/*while (cin>>line)
+		myfile_in<<line;*/
+	while(myfile_out.getline(line,180))
+		myfile_in<<line<<endl;
+	myfile_in.close();
+	return 0;
+
+
+}
--- helloworld/helloworld/helloworld.cpp
+++ helloworld/helloworld/helloworld.cpp
@@ -1,7 +1,20 @@
 #include<iostream>
+#include<fstream>
+
+using namespace std;
 
 int main(){
-	std::cout<<"hello world!i'm Liwenjie!"<<std::endl;
-	std::cout<<"i add something in here~"<<std::endl;
+	char name[8],score[6];
+	ifstream txtfile;
+	txtfile.open("H:\\work\\compiler\\VS2008\\mycode\\helloworld\\helloworld\\out.txt");
+	if(!txtfile){
+		cerr<<"Error!"<<endl;
+		exit(1);
+	}
+	while(!txtfile.eof()){
+		txtfile>>name>>score;
+		cout<<name<<'\t'<<score<<endl;
+	}
+	txtfile.close();
 	return 0;
 }
